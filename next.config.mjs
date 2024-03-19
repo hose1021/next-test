bc528628722b327718095e2ebf4ccfff4df325c8
hose1021 <hose1021@gmail.com>
2024-03-20 00:00:24 +0400

    init

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const config = {
+    reactStrictMode: true,
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+};
+
+export default config;
